9d436e75c5f05c168c1cf6ac81f8b63d2dcba790
BjornGrylls <35100000+BjornGrylls@users.noreply.github.com>
2021-03-25 03:43:19 +0100

    Added http server

--- twitch-schedule.js
+++ twitch-schedule.js
@@ -13,8 +13,8 @@ builder.calname = 'Twitch';
 builder.method = 'REQUEST';
 
 
-async function getSchedule(channelName, displayName) {
-	return axios.post('https://gql.twitch.tv/gql', '[{"operationName":"StreamSchedule","variables":{"login":"' + channelName + '","startingWeekday":"MONDAY","utcOffsetMinutes":120},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"d77c052e84dfeb3ca1c3633b41cda0fa988577a56c519f43d3142bc99de83ac5"}}}]', { headers: { "Client-Id": "kimne78kx3ncx6brgo4mv6wki5h1ko" } })
+async function getSchedule(channelName, callback) {
+	return callback(axios.post('https://gql.twitch.tv/gql', '[{"operationName":"StreamSchedule","variables":{"login":"' + channelName + '","startingWeekday":"MONDAY","utcOffsetMinutes":120},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"d77c052e84dfeb3ca1c3633b41cda0fa988577a56c519f43d3142bc99de83ac5"}}}]', { headers: { "Client-Id": "kimne78kx3ncx6brgo4mv6wki5h1ko" } })
 		.then((res) => {
 			let schedule = res.data[0]["data"]["user"]["channel"]["schedule"]
 			let segments = schedule["segments"]
@@ -60,18 +60,30 @@ async function getSchedule(channelName, displayName) {
 
 		}).catch((err) => {
 			console.error(err);
-		});
+		}));
 }
 
 
-(async () => {
-	await getSchedule("warframe", "Warframe")
-	await getSchedule("warframeinternational", "Warframe International")
+
+const http = require('http');
+
+http.createServer(async (req, res) => {
+	builder.events = []
+
+	const getSchedule1 = (channelName) => new Promise(resolve => getSchedule(
+        channelName,
+        function(res) { resolve(res); }));
+
+	result = await getSchedule1("warframe")
+	//res.toString()
+
+	result = await getSchedule1("warframeinternational")
+	//res.toString()
 
 	var icsFileContent = builder.toString();
-	return icsFileContent;
-	fs.writeFile("twitch.ics", icsFileContent, function (err) {
-		if (err) return console.log(err);
-	  });
-	  
-})();
+
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.write(icsFileContent);
+	res.end();
+
+}).listen(process.env.PORT || 8080);
